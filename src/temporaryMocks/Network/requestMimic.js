ecccfed5654cfd0a0640a5a7ba2486cdb4eaf68a
alitonia <allyofjustice1@gmail.com>
2020-10-15 15:03:54 +0700

    test: add theme

--- src/temporaryMocks/Network/requestMimic.js
+++ src/temporaryMocks/Network/requestMimic.js
@@ -1,8 +1,10 @@
+import { makeId } from '../../lib/util/makeRandomString';
+
 const network_data = (num: number) =>
   [...Array(num).keys()].map((x) => {
     return {
       id: x,
-      name: `name ${x}`,
+      name: `name ${makeId(4)}`,
     };
   });
 
